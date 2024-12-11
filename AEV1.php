868b987a7fe0089997cc40817d5972f3c63e1226
Vitaliy <viluxx@alumnatflorida.es>
2024-12-11 15:45:02 +0100

    +

---
+++ AEV1.php
@@ -0,0 +1,246 @@
+<?php
+$pomodoroHaters = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', 'A', 'A', 'A', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '0', '0', '0', 'A', '0', 'A', 'A', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', 'A', 'A', '0', '0', 'A', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', 'A', 'A', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~']
+];
+$impacts = [
+    [20, 4],
+    [2, 13],
+    [13, 12],
+    [3, 17],
+    [2, 13],
+    [5, 19],
+    [6, 18],
+    [5, 2],
+    [20, 13],
+    [9, 7],
+    [5, 9],
+    [15, 16],
+    [16, 13],
+    [16, 9],
+    [16, 0],
+    [3, 19],
+    [19, 8],
+    [1, 16],
+    [18, 4],
+    [21, 23],
+    [7, 17],
+    [22, 15],
+    [21, 6],
+    [14, 8],
+    [12, 23],
+    [7, 7],
+    [22, 4],
+    [3, 21],
+    [2, 3],
+    [8, 11],
+    [0, 4],
+    [7, 21],
+    [11, 4],
+    [7, 15],
+    [6, 17],
+    [5, 19],
+    [4, 19],
+    [4, 7],
+    [23, 21],
+    [15, 20],
+    [2, 9],
+    [21, 2],
+    [1, 13],
+    [7, 10],
+    [21, 5],
+    [13, 17],
+    [2, 14],
+    [11, 14],
+    [22, 17],
+    [18, 22],
+    [2, 23],
+    [3, 1],
+    [18, 6],
+    [17, 12],
+    [18, 18],
+    [20, 2],
+    [3, 14],
+    [11, 21],
+    [6, 5],
+    [6, 2],
+    [12, 23],
+    [18, 18],
+    [21, 6],
+    [10, 12],
+    [5, 4],
+    [16, 19],
+    [8, 10],
+    [12, 21],
+    [15, 1],
+    [20, 14],
+    [3, 20],
+    [6, 19],
+    [20, 13],
+    [15, 4],
+    [10, 2],
+    [5, 16],
+    [20, 1],
+    [12, 13],
+    [11, 5],
+    [12, 14],
+    [8, 3],
+    [6, 8],
+    [19, 7],
+    [16, 9],
+    [13, 20],
+    [3, 5],
+    [1, 0],
+    [20, 14],
+    [12, 21],
+    [12, 16],
+    [15, 7],
+    [9, 1],
+    [1, 18],
+    [20, 6],
+    [8, 6],
+    [22, 1],
+    [10, 22],
+    [19, 19],
+    [7, 16],
+    [8, 8]
+];
+function mostrarMapa($mapa) {
+    foreach ($mapa as $fila) {
+        echo implode(' ', $fila) . "<br>";
+    }
+}
+
+function marcarImpactosUrbanos($mapa, $impactos) {
+    foreach ($impactos as $impacto) {
+        list($y, $x) = $impacto;
+        if (isset($mapa[$y][$x]) && $mapa[$y][$x] === 'A') {
+            $mapa[$y][$x] = 'C';
+        }
+    }
+    return $mapa;
+}
+
+function calcularPersonasAfectadas($mapa) {
+    $personasPorKm2 = 5000;
+    $personasAfectadas = 0;
+    $colirio = 0.25;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda === 'C') {
+                $personasAfectadas += $personasPorKm2;
+            }
+        }
+    }
+    return $personasAfectadas;
+}
+
+function calcularColirioNecesario($personasAfectadas) {
+    $colirioPorPersona = 25; 
+    return ($personasAfectadas * $colirioPorPersona) / 1000; 
+}
+
+function marcarTodosImpactos($mapa, $impactos) {
+    foreach ($impactos as $impacto) {
+        list($y, $x) = $impacto;
+        if (isset($mapa[$y][$x])) {
+            if ($mapa[$y][$x] === '0') {
+                $mapa[$y][$x] = 'X';
+            } elseif ($mapa[$y][$x] === '~') {
+                $mapa[$y][$x] = 'S';
+            }
+        }
+    }
+    return $mapa;
+}
+
+function calcularDañosEconomicos($mapa) {
+    $dañoUrbanoPorKm2 = 200000;
+    $dañoNoUrbanoPorKm2 = 50000;
+    $dañoUrbano = $dañoNoUrbano = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda === 'C') {
+                $dañoUrbano += $dañoUrbanoPorKm2;
+            } elseif ($celda === 'X') {
+                $dañoNoUrbano += $dañoNoUrbanoPorKm2;
+            }
+        }
+    }
+    return [$dañoUrbano + $dañoNoUrbano, $dañoUrbano, $dañoNoUrbano];
+}
+
+function calcularMar($mapa) {
+    $marTotal = 0;
+    $marAfectado = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda === '~') {
+                $marTotal++;
+            } elseif ($celda === 'S') {
+                $marAfectado++;
+            }
+        }
+    }
+    return [$marTotal, $marAfectado];
+}
+
+function calcularRecaudacion($marAfectado) {
+    $pescadoPorKm2 = 2000; 
+    $precioPorKg = 5; 
+    return $marAfectado * $pescadoPorKm2 * 1000 * $precioPorKg; 
+}
+
+echo "Mapa original:<br><br>";
+mostrarMapa($pomodoroHaters);
+
+$mapaConUrbanosAfectados = marcarImpactosUrbanos($pomodoroHaters, $impacts);
+echo "<br><br>Mapa con núcleos urbanos afectados:<br><br>";
+mostrarMapa($mapaConUrbanosAfectados);
+
+$personasAfectadas = calcularPersonasAfectadas($mapaConUrbanosAfectados);
+echo "<br>Personas afectadas: $personasAfectadas<br>";
+
+$litrosColirio = calcularColirioNecesario($personasAfectadas);
+echo "Se necesitan $litrosColirio litros de colirio.<br>";
+
+$mapaConTodosImpactos = marcarTodosImpactos($mapaConUrbanosAfectados, $impacts);
+echo "<br>Mapa con todos los impactos:<br><br>";
+mostrarMapa($mapaConTodosImpactos);
+
+list($dañoTotal, $dañoUrbano, $dañoNoUrbano) = calcularDañosEconomicos($mapaConTodosImpactos);
+echo "<br>Daños económicos:<br>";
+echo "Total: $dañoTotal €<br>";
+echo "Zona urbana: $dañoUrbano €<br>";
+echo "Zona no urbana: $dañoNoUrbano €<br>";
+
+list($marTotal, $marAfectado) = calcularMar($mapaConTodosImpactos);
+echo "<br>Superficie de mar:<br>";
+echo "Mar total: $marTotal km2<br>";
+echo "Mar afectado: $marAfectado km2<br>";
+
+$recaudacion = calcularRecaudacion($marAfectado);
+echo "<br>Recaudación esperada por la ONG: $recaudacion €<br>";
+?>
